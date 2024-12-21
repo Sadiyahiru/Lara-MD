39b5bbf51a73026ddd878481de61ddf538bcdfcd
Sadiyahiru <sadees913@gmail.com>
2024-12-21 16:50:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "uv4zQLYQ#c3xaT7GkVimrwXtFSMcZE9aO5tgiYQING5xtGgjiIZs", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
